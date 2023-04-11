ceb540b8e250c3297a8594f4adb6eb0994786d2a
Sergey Zhmurin <s.zhmurin@innopolis.ru>
2023-04-11 20:45:41 +0300

    global accel without gravity

--- mpu9250/pi_imu_receiver.py
+++ mpu9250/pi_imu_receiver.py
@@ -12,6 +12,15 @@ socket = context.socket(zmq.SUB)
 socket.connect(url)
 socket.setsockopt(zmq.SUBSCRIBE, b'')
 
+g_vec = np.array([0.0, 0.0, 9.81])
+
+
+def get_accel_global(rpy, acc):
+    acc_rot = Rot.from_euler('xyz', rpy, degrees=True)
+    accel_g = acc_rot.apply(np.array([acc[1], acc[0], acc[2]]))
+    accel_global = accel_g - np.array([0.0, 0.0, 9.81])
+    return accel_global
+
 
 def rf(val):
     return round(float(val), 2)
@@ -26,8 +35,7 @@ while True:
     acc = np.array(json.loads(pack_acc))
     print('ACC', float(acc[0]), float(acc[1]), float(acc[2]))
 
-    acc_rot = Rot.from_euler('xyz', rpy, degrees=True)
-    accel_glob = acc_rot.apply(np.array([acc[1], acc[0], acc[2]]))
-    # print(accel_glob)
+    accel_glob = get_accel_global(rpy, acc)
+    print(accel_glob)
 
 
