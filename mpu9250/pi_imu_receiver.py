3a807cfeb3309a0348e36839b1e092ee92d9048c
Sergey Zhmurin <s.zhmurin@innopolis.ru>
2023-04-11 13:01:24 +0300

    fload in print

--- mpu9250/pi_imu_receiver.py
+++ mpu9250/pi_imu_receiver.py
@@ -21,10 +21,10 @@ while True:
     packet = socket.recv_json(0)
     pack_rpy = packet['rpy']
     rpy = np.array(json.loads(pack_rpy, parse_float=rf))
-    # print('RPY ', rpy)
+    print('RPY ', float(rpy[0]), float(rpy[1]), float(rpy[2]))
     pack_acc = packet['acc']
     acc = np.array(json.loads(pack_acc))
-    print('ACC', acc)
+    print('ACC', float(acc[0]), float(acc[1]), float(acc[2]))
 
     acc_rot = Rot.from_euler('xyz', rpy, degrees=True)
     accel_glob = acc_rot.apply(np.array([acc[1], acc[0], acc[2]]))
