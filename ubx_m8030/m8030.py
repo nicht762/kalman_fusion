6bde7e3896c4e5c2106c1913d1a0074af594d502
nicht762 <nicht762@gmail.com>
2023-04-18 15:08:50 +0300

    geo2enu conversion

--- ubx_m8030/m8030.py
+++ ubx_m8030/m8030.py
@@ -1,5 +1,12 @@
+import pymap3d
 from serial import Serial
 from pyubx2 import UBXReader
+
+ellipsoid = pymap3d.Ellipsoid()
+# base
+lat_0 = 55.760057829964516
+lon_0 = 48.75896086562219
+hgt_0 = 199.37
 
 stream = Serial('COM20', 115200, timeout=3)
 ubr = UBXReader(stream)
@@ -7,6 +14,9 @@ ubr = UBXReader(stream)
 # (raw_data, parsed_data) = ubr.read()
 for (raw_data, parsed_data) in ubr:
     print(parsed_data)
+    east, north, up = pymap3d.geodetic2enu(lat_1, lon_1, hgt_1,
+                                           lat_0, lon_0, hgt_0,
+                                           ell=ellipsoid, deg=True)
 
 # NMEA/GNGGA
 # <NMEA(GNGGA, time=22:11:25, lat=55.7490593333, NS=N, lon=48.7426426667, EW=E,
