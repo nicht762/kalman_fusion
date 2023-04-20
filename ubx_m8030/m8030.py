a04e6bbdd3669cad3268bd0089df404ab2c5ca15
nicht762 <nicht762@gmail.com>
2023-04-20 14:54:47 +0300

    parsed gnss data

--- ubx_m8030/m8030.py
+++ ubx_m8030/m8030.py
@@ -1,6 +1,7 @@
 import pymap3d
 from serial import Serial
-from pyubx2 import UBXReader
+# from pyubx2 import UBXReader
+from pynmeagps import NMEAReader, NMEAMessage
 
 ellipsoid = pymap3d.Ellipsoid()
 # base
@@ -8,15 +9,18 @@ lat_0 = 55.760057829964516
 lon_0 = 48.75896086562219
 hgt_0 = 199.37
 
-stream = Serial('COM20', 115200, timeout=3)
-ubr = UBXReader(stream)
+# stream = Serial('COM20', 115200, timeout=3)
+# ubr = UBXReader(stream)
+stream = Serial('/dev/ttyACM0', 115200, timeout=3)
+nmr = NMEAReader(stream)
 
-# (raw_data, parsed_data) = ubr.read()
-for (raw_data, parsed_data) in ubr:
-    print(parsed_data)
-    east, north, up = pymap3d.geodetic2enu(lat_1, lon_1, hgt_1,
-                                           lat_0, lon_0, hgt_0,
-                                           ell=ellipsoid, deg=True)
+for (raw_data, parsed_data) in nmr:
+    if parsed_data.msgID == 'GGA':
+        print(parsed_data)
+        east, north, up = pymap3d.geodetic2enu(parsed_data.lat, parsed_data.lon, parsed_data.alt,
+                                               lat_0, lon_0, hgt_0,
+                                               ell=ellipsoid, deg=True)
+        print(east, north, up)
 
 # NMEA/GNGGA
 # <NMEA(GNGGA, time=22:11:25, lat=55.7490593333, NS=N, lon=48.7426426667, EW=E,
